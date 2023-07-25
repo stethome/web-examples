dd7b0a402cb70330e396d57c0bc34b55454a1b95
Filip Maciejewski <maciejewski@stethome.com>
2023-07-25 02:41:37 +0200

    Refactor UserProvider, add filtering and pagination

--- api-platform/api/src/User/ApiResource/UserModel.php
+++ api-platform/api/src/User/ApiResource/UserModel.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\User\ApiResource;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
@@ -14,19 +17,27 @@ use App\User\State\UserRegisterProcessor;
 use Symfony\Component\Uid\Uuid;
 
 #[Post(input: UserRegisterDto::class, processor: UserRegisterProcessor::class)]
-#[Get(provider: UserProvider::class)]
-#[GetCollection(provider: UserProvider::class)]
+#[Get(
+    uriVariables: ['uuid'],
+    provider: UserProvider::class,
+    stateOptions: new Options(SecurityUser::class),
+)]
+#[GetCollection(
+    provider: UserProvider::class,
+    stateOptions: new Options(SecurityUser::class)
+)]
+#[ApiFilter(SearchFilter::class, properties: ['email' => 'partial'])]
 class UserModel
 {
-    public Uuid $id;
+    public Uuid $uuid;
     public string $email;
     public string $name;
     public string $surname;
 
     private function __construct(
-        Uuid $id,
+        Uuid $uuid,
     ) {
-        $this->id = $id;
+        $this->uuid = $uuid;
     }
 
     public static function fromSecurityUser(SecurityUser $securityUser): self
--- api-platform/api/src/User/State/UserProvider.php
+++ api-platform/api/src/User/State/UserProvider.php
@@ -4,35 +4,57 @@ declare(strict_types=1);
 
 namespace App\User\State;
 
+use ApiPlatform\Doctrine\Orm\Paginator;
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
 use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\Pagination\PaginatorInterface;
+use ApiPlatform\State\Pagination\TraversablePaginator;
 use ApiPlatform\State\ProviderInterface;
 use App\User\ApiResource\UserModel;
-use App\User\Repository\SecurityUserRepository;
 
 final readonly class UserProvider implements ProviderInterface
 {
     public function __construct(
-        private SecurityUserRepository $repository,
+        private ItemProvider $itemProvider,
+        private CollectionProvider $collectionProvider,
     ) {
     }
 
-    public function provide(Operation $operation, array $uriVariables = [], array $context = []): UserModel|\Generator|null
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): UserModel|PaginatorInterface|null
     {
         if ($operation instanceof CollectionOperationInterface) {
-            // @todo filters, pagination
-            return $this->provideCollection();
+            return $this->provideCollection($operation, $uriVariables, $context);
         }
 
-        $user = $this->repository->find($uriVariables['id']);
+        return $this->provideItem($operation, $uriVariables, $context);
+    }
+
+    private function provideCollection(Operation $operation, array $uriVariables, array $context): PaginatorInterface
+    {
+        /** @var Paginator $paginator */
+        $paginator = $this->collectionProvider->provide($operation, $uriVariables, $context);
+
+        return new TraversablePaginator(
+            $this->mapPaginator($paginator),
+            $paginator->getCurrentPage(),
+            $paginator->getItemsPerPage(),
+            $paginator->getTotalItems(),
+        );
+    }
+
+    private function provideItem(Operation $operation, array $uriVariables, array $context): ?UserModel
+    {
+        $user = $this->itemProvider->provide($operation, $uriVariables, $context);
 
         return $user ? UserModel::fromSecurityUser($user) : null;
     }
 
-    private function provideCollection(): \Generator
+    private function mapPaginator(Paginator $paginator): \Generator
     {
-        foreach ($this->repository->findAll() as $user) {
-            yield UserModel::fromSecurityUser($user);
+        foreach ($paginator as $item) {
+            yield UserModel::fromSecurityUser($item);
         }
     }
 }
