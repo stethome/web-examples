cc3f79d8b627aee79576f35b2cbb437f81fb3915
Filip Maciejewski <maciejewski@stethome.com>
2023-08-03 20:55:43 +0200

    Add custom layout for /app

--- api-platform/pwa/components/common/Layout.tsx
+++ api-platform/pwa/components/common/Layout.tsx
@@ -5,6 +5,8 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "react-query";
+import {useRouter} from "next/router";
+import NavBar from "../app/NavBar";
 
 const Layout = ({
   children,
@@ -14,6 +16,38 @@ const Layout = ({
   dehydratedState: DehydratedState;
 }) => {
   const [queryClient] = useState(() => new QueryClient());
+  const router = useRouter();
+
+  if (router.pathname.startsWith('/app')) {
+    return (<>
+      <div className="w-full flex flex-col sm:flex-row flex-grow overflow-hidden">
+        <div className="sm:w-1/3 md:1/4 w-full flex-shrink flex-grow-0 p-4">
+          <div className="sticky top-0 p-4 w-full bg-gray-100 rounded">
+            <NavBar />
+          </div>
+        </div>
+        <main role="main" className="w-full h-full flex-grow p-3 overflow-auto">
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={dehydratedState}>{children}</Hydrate>
+          </QueryClientProvider>
+        </main>
+      </div>
+      <footer className="bg-blue-500 mt-auto">
+        <div className="px-4 py-1 text-white mx-auto">
+          <h1 className="text-2xl hidden sm:block mb-2">Footer</h1>
+          <div className="flex">
+            <div className="flex flex-col">
+              <a href="https://stethome.com" className="text-xs uppercase tracking-wider py-2">Website</a>
+              <a href="https://github.com/stethome" className="text-xs uppercase tracking-wider py-2">GitHub</a>
+            </div>
+          </div>
+          <div className="text-right text-xs py-2">
+            <span>&copy;2023 Stethome</span>
+          </div>
+        </div>
+      </footer>
+    </>)
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
