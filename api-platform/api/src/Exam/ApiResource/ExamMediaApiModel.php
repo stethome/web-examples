bac617ae78e543348617661b4dd63a7ae1815fa9
Filip Maciejewski <maciejewski@stethome.com>
2023-08-08 21:41:48 +0200

    Code format

--- api-platform/api/src/Exam/ApiResource/ExamMediaApiModel.php
+++ api-platform/api/src/Exam/ApiResource/ExamMediaApiModel.php
@@ -7,7 +7,6 @@ namespace App\Exam\ApiResource;
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
 use App\Exam\Entity\Exam;
 use App\Exam\Service\State\ExamMediaApiTokenProvider;
 
